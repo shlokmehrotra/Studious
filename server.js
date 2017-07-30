6839af0840b7c85fc661fdac7ab7b1bc650120d5
Prem Dhoot <premdhoot6@gmail.com>
2017-07-30 12:16:12 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var sjcl = require('sjcl');
+var randomstring = require('randomstring');
+//var nodemailer = require('nodemailer);
+var jade = require('jade');
+var express = require('express');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host:'localhost',
+    user:'shlokmehrotra',
+    password:'',
+    database:'c9'
+});
+console.log("Connected to database");
+var app = express();
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(app.router);
+});
+//The port number of the c9 platform is process.env.PORT
+//On the computer, it's localhost and not process.env.PORT
+app.get('/', function(req, res){
+    res.render('index.html');
+    //jade.renderFile('signup.jade', {"username":"", "password":""});
+});
+app.get('/chat.html', function(req, res){
+    res.send('<h1>Hello World</h1>');
+});
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index/html');
+});
